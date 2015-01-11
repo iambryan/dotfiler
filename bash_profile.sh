69036a0f20a569f077c91dc73cc513813c86939b
Bryan Sutherland <bryan.sutherland@gmail.com>
2015-01-11 15:13:07 -0500

    Created a shell for bash_profile

---
+++ bash_profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -   
+#title          :bash_profile.sh
+#description    :dotfiler's default .bash_profile
+#author         :http://github.com/iambryan
+#date           :20150111
+#version        :0.1    
+#usage          :./bash_profile.sh
+#notes          :       
+#bash_version   :4.2.53(1)-release
+#============================================================================
+
+
