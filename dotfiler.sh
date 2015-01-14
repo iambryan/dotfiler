8cca80740bbb58b4057cd455a8492b69ff56605b
Bryan Sutherland <bryan.sutherland@gmail.com>
2015-01-14 11:44:42 -0500

    Adding installation detection and install routine

--- dotfiler.sh
+++ dotfiler.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
-if [ -f /etc/bashrc ]; then
+
+
+if [ -f /etc/bashrc ] || [ /etc/bash.bashrc ]; then
 source /etc/bashrc
 fi
-for dotfiles in $(ls ${HOME}/.dotfiler/*.sh) ; do
-source $dotfiles
-done
+
+
+
+if [ -d ~/.dotfiler ]; then
+  for dotfiles in $(ls ${HOME}/.dotfiler/*.sh) ; do
+    source $dotfiles
+  done
+else
+  read #Read input to approve installation
+
+
+
+fi
+
+
+
+
+
 unset dotfiles
+
