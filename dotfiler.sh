982a2406b8241212738386fe5758e3950c4b989c
Bryan Sutherland <bryan.sutherland@gmail.com>
2015-10-04 03:05:08 -0400

    updates to _prompts and dotfiler.sh

--- dotfiler.sh
+++ dotfiler.sh
@@ -1,9 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash  
+#title          :dotfiler.sh
+#description    :BASH environment customization script
+#author         :http://github.com/iambryan
+#date           :20151004
+#version        :0.1    
+#notes          :dotfiler.sh will prepare a custom BASH environment for you, then
+# will set your configuration in stone. Heck, it'll even (at some point),
+# backup your configs too!!!
+#============================================================================
 
 
-if [ -f /etc/bashrc ] || [ /etc/bash.bashrc ]; then
-source /etc/bashrc
-fi
 
 
 
@@ -15,24 +21,16 @@ else
   read install
 
   case $install in
-  	y|Y )
-		echo "Installing DotFiler to "
-  		;;
-	n|N )
+  	#y|Y )
+		#echo "Installing DotFiler to "
+  	#	;;
+	  y|Y|n|N )
 		echo "Leaving your files alone now"
 		;;
-	* )
+	   * )
 		echo "Leaving your files alone now";
 		exit
   esac
-
-
-
 fi
-
-
-
-
-
 unset dotfiles
 
